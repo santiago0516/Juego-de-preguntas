f83b36074b435a2c027c1119386a942728f81cf3
Laura Bolaños <santiagoerazo11116.gmail.com>
2022-12-03 00:22:15 -0500

    El proyecto se ha terminado

---
+++ base_de_preguntas.js
@@ -0,0 +1,62 @@
+/*function LaTeX(code) {
+  return `https://latex.codecogs.com/svg.image?\\large&space;${code}`;
+}
+
+function opnLaTeX(code) {
+  return `<img src="${LaTeX(code)}" style="height:40px">`;
+}*/
+
+let baseDePreguntas = [
+  {
+    pregunta: "¿Cuantos litros de sangre tiene una persona adulta?",
+    respuesta: "4 y 6 litros",
+    distractores: ["0,5 litros", "10 litros", "2 y 4"],
+  },
+  {
+    pregunta: "La sal común esta formada por dos elementos, ¿cuales son?",
+    respuesta: "Sodio y cloro",
+    distractores: ["sodio y potasio", "sodio y carbono", "potasio y cloro"],
+  },
+  {
+    pregunta: "¿Cual es el libro más vendido despues de la Biblia?",
+    respuesta: "Don Quijote de la Mancha",
+    distractores: ["A Brave New World", "Farenheit 451", "Cien Años de Soledad"],
+  },
+  {
+    pregunta: "¿Cual es el país más grande y el más pequeño del mundo?",
+    respuesta: "Rusia y Vaticano",
+    distractores: ["Canada y Mónaco", "Estados Unidos y Japon", "Colombia y Venezuela"],
+  },
+  {
+    pregunta: "¿Quien es el autor de la frase 'pienso, luego existo'(cogito ergo sum)?",
+    respuesta: "Rene Descartes",
+    distractores: ["Friedrich Nietzsche", "Friedrich Hegel", "Platon"],
+  },
+  {
+    pregunta: "¿Cual es la altura de la red de voleibol en los juegos masculino y femenino?",
+    respuesta: "2,43m y 2,24m",
+    distractores: ["2,4m para ambos", "1,8m y 1,5m", "2,45m y 2,67m"],
+  },
+  {
+    pregunta: "¿Cuanto tiempo tarda la luz del Sol en llegar a la Tierra?",
+    respuesta: "8 minutos",
+    distractores: ["7 minutos", "6 minutos", "17,5 segundos"],
+  },
+  {
+    pregunta: "¿Cuantos jugadores por equipo participan en un partido de voleibol?",
+    respuesta: "6",
+    distractores: ["8", "10", "12"],
+  },
+  {
+    pregunta: "¿A quien se le atribuye la frase:'Solo sé que no sé nada'?",
+    respuesta: "Socrates",
+    distractores: ["Aristoteles", "Epicuro", "Séneca"],
+  },
+  {
+    pregunta: "¿En que periodo de la prehistoria fue descubierto el fuego?",
+    respuesta: "Paleolitico",
+    distractores: ["Edad de piedra", "Edad media", "Neolitico"],
+  },
+];
+
+
--- proyecto.js
+++ proyecto.js
@@ -1,1 +1,58 @@
-console.log("Hello World");
+let INDEX_PREGUNTA = 0;
+var puntaje = 0;
+
+cargarPregunta(INDEX_PREGUNTA);
+
+function cargarPregunta(index) {
+  objetoPregunta = baseDePreguntas[index];
+
+  opciones = [...objetoPregunta.distractores];
+  opciones.push(objetoPregunta.respuesta);
+  for (let i = 0; i < 4; i++) {
+    opciones.sort(() => Math.random() - 0.5);
+  }
+  
+  document.getElementById("pregunta").innerHTML = objetoPregunta.pregunta;
+  document.getElementById("opcion-1").innerHTML = opciones[0];
+  document.getElementById("opcion-2").innerHTML = opciones[1];
+  document.getElementById("opcion-3").innerHTML = opciones[2];
+  document.getElementById("opcion-4").innerHTML = opciones[3];
+}
+
+async function seleccionarOpción(index) {
+  let validezRespuesta = opciones[index] == objetoPregunta.respuesta;
+  if (validezRespuesta) {
+    await Swal.fire({
+      title: "Respuesta correcta",
+      text: "La respuesta ha sido correcta",
+      icon: "success",
+    });
+    puntaje=puntaje+4;
+  } else {
+    await Swal.fire({
+      title: "Respuesta Incorrecta",
+      html: `La respuesta correcta es ${objetoPregunta.respuesta}`,
+      icon: "error",
+    });
+    puntaje=puntaje-2;
+  }
+  INDEX_PREGUNTA++;
+  if (INDEX_PREGUNTA >= baseDePreguntas.length) {
+    await Swal.fire({
+      title: "Juego términado",
+      text: `Tu puntaje fue de: ${puntaje}`,
+    });
+
+    if(puntaje>20)
+    {
+      alert("Ganaste");
+    }
+    else{
+      alert("perdiste");
+    }
+
+    INDEX_PREGUNTA = 0;
+    puntaje = 0;
+  }
+  cargarPregunta(INDEX_PREGUNTA);
+}
